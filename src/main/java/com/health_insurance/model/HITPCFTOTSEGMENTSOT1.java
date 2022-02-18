08d7842f9a1dcf61bc217aae0aabebf556352a66
system <system>
2022-02-18 14:32:40 -0500

    {/src/main/java/com/health_insurance/model/HITPCFTOTSEGMENTSOT1.java}

---
+++ src/main/java/com/health_insurance/model/HITPCFTOTSEGMENTSOT1.java
@@ -0,0 +1,112 @@
+package com.health_insurance.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class HITPCFTOTSEGMENTSOT1 implements java.io.Serializable {
+
+    static final long serialVersionUID = 1L;
+
+    public HITPCFTOTSEGMENTSOT1() {
+    }
+
+    @JsonProperty("HIT_PCF_SEGMENT_TYPE")
+    private String HIT_PCF_SEGMENT_TYPE;
+    @JsonProperty("HIT_OT1_PAYMENT_METHOD")
+    private String HIT_OT1_PAYMENT_METHOD;
+    @JsonProperty("HIT_OT1_TOT_ALLOWED_AMT")
+    private String HIT_OT1_TOT_ALLOWED_AMT;
+    @JsonProperty("HIT_OT1_TOT_COPAY_AMT")
+    private String HIT_OT1_TOT_COPAY_AMT;
+    @JsonProperty("HIT_OT1_TOT_COINS_AMT")
+    private String HIT_OT1_TOT_COINS_AMT;
+    @JsonProperty("HIT_OT1_PROC_CODE_MOD3")
+    private String HIT_OT1_PROC_CODE_MOD3;
+    @JsonProperty("HIT_OT1_PROC_CODE_MOD4")
+    private String HIT_OT1_PROC_CODE_MOD4;
+    @JsonProperty("HIT_OT1_TREATMENT_AUTH_CODE")
+    private String HIT_OT1_TREATMENT_AUTH_CODE;
+    @JsonProperty("HIT_OT1_ORIG_PAT_STATUS")
+    private String HIT_OT1_ORIG_PAT_STATUS;
+    @JsonProperty("HIT_OT1_DOD")
+    private String HIT_OT1_DOD;
+
+    public String getHIT_PCF_SEGMENT_TYPE() {
+        return HIT_PCF_SEGMENT_TYPE;
+    }
+
+    public void setHIT_PCF_SEGMENT_TYPE(String HIT_PCF_SEGMENT_TYPE) {
+        this.HIT_PCF_SEGMENT_TYPE = HIT_PCF_SEGMENT_TYPE;
+    }
+
+    public String getHIT_OT1_PAYMENT_METHOD() {
+        return HIT_OT1_PAYMENT_METHOD;
+    }
+
+    public void setHIT_OT1_PAYMENT_METHOD(String HIT_OT1_PAYMENT_METHOD) {
+        this.HIT_OT1_PAYMENT_METHOD = HIT_OT1_PAYMENT_METHOD;
+    }
+
+    public String getHIT_OT1_TOT_ALLOWED_AMT() {
+        return HIT_OT1_TOT_ALLOWED_AMT;
+    }
+
+    public void setHIT_OT1_TOT_ALLOWED_AMT(String HIT_OT1_TOT_ALLOWED_AMT) {
+        this.HIT_OT1_TOT_ALLOWED_AMT = HIT_OT1_TOT_ALLOWED_AMT;
+    }
+
+    public String getHIT_OT1_TOT_COPAY_AMT() {
+        return HIT_OT1_TOT_COPAY_AMT;
+    }
+
+    public void setHIT_OT1_TOT_COPAY_AMT(String HIT_OT1_TOT_COPAY_AMT) {
+        this.HIT_OT1_TOT_COPAY_AMT = HIT_OT1_TOT_COPAY_AMT;
+    }
+
+    public String getHIT_OT1_TOT_COINS_AMT() {
+        return HIT_OT1_TOT_COINS_AMT;
+    }
+
+    public void setHIT_OT1_TOT_COINS_AMT(String HIT_OT1_TOT_COINS_AMT) {
+        this.HIT_OT1_TOT_COINS_AMT = HIT_OT1_TOT_COINS_AMT;
+    }
+
+    public String getHIT_OT1_PROC_CODE_MOD3() {
+        return HIT_OT1_PROC_CODE_MOD3;
+    }
+
+    public void setHIT_OT1_PROC_CODE_MOD3(String HIT_OT1_PROC_CODE_MOD3) {
+        this.HIT_OT1_PROC_CODE_MOD3 = HIT_OT1_PROC_CODE_MOD3;
+    }
+
+    public String getHIT_OT1_PROC_CODE_MOD4() {
+        return HIT_OT1_PROC_CODE_MOD4;
+    }
+
+    public void setHIT_OT1_PROC_CODE_MOD4(String HIT_OT1_PROC_CODE_MOD4) {
+        this.HIT_OT1_PROC_CODE_MOD4 = HIT_OT1_PROC_CODE_MOD4;
+    }
+
+    public String getHIT_OT1_TREATMENT_AUTH_CODE() {
+        return HIT_OT1_TREATMENT_AUTH_CODE;
+    }
+
+    public void setHIT_OT1_TREATMENT_AUTH_CODE(String HIT_OT1_TREATMENT_AUTH_CODE) {
+        this.HIT_OT1_TREATMENT_AUTH_CODE = HIT_OT1_TREATMENT_AUTH_CODE;
+    }
+
+    public String getHIT_OT1_ORIG_PAT_STATUS() {
+        return HIT_OT1_ORIG_PAT_STATUS;
+    }
+
+    public void setHIT_OT1_ORIG_PAT_STATUS(String HIT_OT1_ORIG_PAT_STATUS) {
+        this.HIT_OT1_ORIG_PAT_STATUS = HIT_OT1_ORIG_PAT_STATUS;
+    }
+
+    public String getHIT_OT1_DOD() {
+        return HIT_OT1_DOD;
+    }
+
+    public void setHIT_OT1_DOD(String HIT_OT1_DOD) {
+        this.HIT_OT1_DOD = HIT_OT1_DOD;
+    }
+}
